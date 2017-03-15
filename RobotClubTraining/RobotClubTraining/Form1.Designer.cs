d67342b3977b8d91db72f20a3eecaa07d820ae7a
Keith_Hancock <Keith_Hancock@usd323.local>
2017-03-15 16:02:40 -0500

    Baseline with transient files ignored.

---
+++ RobotClubTraining/RobotClubTraining/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace RobotClubTraining
+{
+    partial class frmRobotClub
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdPersonA = new System.Windows.Forms.Button();
+            this.cmdPersonB = new System.Windows.Forms.Button();
+            this.cmdPersonC = new System.Windows.Forms.Button();
+            this.cmdPersonD = new System.Windows.Forms.Button();
+            this.lblPersonA = new System.Windows.Forms.Label();
+            this.txtPersonA = new System.Windows.Forms.TextBox();
+            this.txtPersonB = new System.Windows.Forms.TextBox();
+            this.txtPersonC = new System.Windows.Forms.TextBox();
+            this.txtPersonD = new System.Windows.Forms.TextBox();
+            this.lblPersonB = new System.Windows.Forms.Label();
+            this.lblPersonC = new System.Windows.Forms.Label();
+            this.lblPersonD = new System.Windows.Forms.Label();
+            this.cmdExample = new System.Windows.Forms.Button();
+            this.txtExample = new System.Windows.Forms.TextBox();
+            this.lblExample = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmdPersonA
+            // 
+            this.cmdPersonA.Location = new System.Drawing.Point(12, 12);
+            this.cmdPersonA.Name = "cmdPersonA";
+            this.cmdPersonA.Size = new System.Drawing.Size(100, 23);
+            this.cmdPersonA.TabIndex = 0;
+            this.cmdPersonA.Text = "Person A";
+            this.cmdPersonA.UseVisualStyleBackColor = true;
+            // 
+            // cmdPersonB
+            // 
+            this.cmdPersonB.Location = new System.Drawing.Point(118, 12);
+            this.cmdPersonB.Name = "cmdPersonB";
+            this.cmdPersonB.Size = new System.Drawing.Size(100, 23);
+            this.cmdPersonB.TabIndex = 1;
+            this.cmdPersonB.Text = "Person B";
+            this.cmdPersonB.UseVisualStyleBackColor = true;
+            // 
+            // cmdPersonC
+            // 
+            this.cmdPersonC.Location = new System.Drawing.Point(224, 12);
+            this.cmdPersonC.Name = "cmdPersonC";
+            this.cmdPersonC.Size = new System.Drawing.Size(100, 23);
+            this.cmdPersonC.TabIndex = 2;
+            this.cmdPersonC.Text = "Person C";
+            this.cmdPersonC.UseVisualStyleBackColor = true;
+            // 
+            // cmdPersonD
+            // 
+            this.cmdPersonD.Location = new System.Drawing.Point(330, 12);
+            this.cmdPersonD.Name = "cmdPersonD";
+            this.cmdPersonD.Size = new System.Drawing.Size(100, 23);
+            this.cmdPersonD.TabIndex = 3;
+            this.cmdPersonD.Text = "Person D";
+            this.cmdPersonD.UseVisualStyleBackColor = true;
+            // 
+            // lblPersonA
+            // 
+            this.lblPersonA.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblPersonA.Location = new System.Drawing.Point(12, 64);
+            this.lblPersonA.Name = "lblPersonA";
+            this.lblPersonA.Size = new System.Drawing.Size(100, 23);
+            this.lblPersonA.TabIndex = 4;
+            // 
+            // txtPersonA
+            // 
+            this.txtPersonA.Location = new System.Drawing.Point(12, 41);
+            this.txtPersonA.Name = "txtPersonA";
+            this.txtPersonA.Size = new System.Drawing.Size(100, 20);
+            this.txtPersonA.TabIndex = 5;
+            // 
+            // txtPersonB
+            // 
+            this.txtPersonB.Location = new System.Drawing.Point(118, 41);
+            this.txtPersonB.Name = "txtPersonB";
+            this.txtPersonB.Size = new System.Drawing.Size(100, 20);
+            this.txtPersonB.TabIndex = 6;
+            // 
+            // txtPersonC
+            // 
+            this.txtPersonC.Location = new System.Drawing.Point(224, 41);
+            this.txtPersonC.Name = "txtPersonC";
+            this.txtPersonC.Size = new System.Drawing.Size(100, 20);
+            this.txtPersonC.TabIndex = 7;
+            // 
+            // txtPersonD
+            // 
+            this.txtPersonD.Location = new System.Drawing.Point(330, 41);
+            this.txtPersonD.Name = "txtPersonD";
+            this.txtPersonD.Size = new System.Drawing.Size(100, 20);
+            this.txtPersonD.TabIndex = 8;
+            // 
+            // lblPersonB
+            // 
+            this.lblPersonB.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblPersonB.Location = new System.Drawing.Point(118, 64);
+            this.lblPersonB.Name = "lblPersonB";
+            this.lblPersonB.Size = new System.Drawing.Size(100, 23);
+            this.lblPersonB.TabIndex = 9;
+            // 
+            // lblPersonC
+            // 
+            this.lblPersonC.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblPersonC.Location = new System.Drawing.Point(224, 64);
+            this.lblPersonC.Name = "lblPersonC";
+            this.lblPersonC.Size = new System.Drawing.Size(100, 23);
+            this.lblPersonC.TabIndex = 10;
+            // 
+            // lblPersonD
+            // 
+            this.lblPersonD.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblPersonD.Location = new System.Drawing.Point(330, 64);
+            this.lblPersonD.Name = "lblPersonD";
+            this.lblPersonD.Size = new System.Drawing.Size(100, 23);
+            this.lblPersonD.TabIndex = 11;
+            // 
+            // cmdExample
+            // 
+            this.cmdExample.Location = new System.Drawing.Point(118, 90);
+            this.cmdExample.Name = "cmdExample";
+            this.cmdExample.Size = new System.Drawing.Size(206, 23);
+            this.cmdExample.TabIndex = 12;
+            this.cmdExample.Text = "Example";
+            this.cmdExample.UseVisualStyleBackColor = true;
+            // 
+            // txtExample
+            // 
+            this.txtExample.Location = new System.Drawing.Point(118, 119);
+            this.txtExample.Name = "txtExample";
+            this.txtExample.Size = new System.Drawing.Size(206, 20);
+            this.txtExample.TabIndex = 13;
+            // 
+            // lblExample
+            // 
+            this.lblExample.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblExample.Location = new System.Drawing.Point(118, 142);
+            this.lblExample.Name = "lblExample";
+            this.lblExample.Size = new System.Drawing.Size(206, 23);
+            this.lblExample.TabIndex = 14;
+            // 
+            // frmRobotClub
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 180);
+            this.Controls.Add(this.lblExample);
+            this.Controls.Add(this.txtExample);
+            this.Controls.Add(this.cmdExample);
+            this.Controls.Add(this.lblPersonD);
+            this.Controls.Add(this.lblPersonC);
+            this.Controls.Add(this.lblPersonB);
+            this.Controls.Add(this.txtPersonD);
+            this.Controls.Add(this.txtPersonC);
+            this.Controls.Add(this.txtPersonB);
+            this.Controls.Add(this.txtPersonA);
+            this.Controls.Add(this.lblPersonA);
+            this.Controls.Add(this.cmdPersonD);
+            this.Controls.Add(this.cmdPersonC);
+            this.Controls.Add(this.cmdPersonB);
+            this.Controls.Add(this.cmdPersonA);
+            this.Name = "frmRobotClub";
+            this.Text = "Robotics Club Thing";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdPersonA;
+        private System.Windows.Forms.Button cmdPersonB;
+        private System.Windows.Forms.Button cmdPersonC;
+        private System.Windows.Forms.Button cmdPersonD;
+        private System.Windows.Forms.Label lblPersonA;
+        private System.Windows.Forms.TextBox txtPersonA;
+        private System.Windows.Forms.TextBox txtPersonB;
+        private System.Windows.Forms.TextBox txtPersonC;
+        private System.Windows.Forms.TextBox txtPersonD;
+        private System.Windows.Forms.Label lblPersonB;
+        private System.Windows.Forms.Label lblPersonC;
+        private System.Windows.Forms.Label lblPersonD;
+        private System.Windows.Forms.Button cmdExample;
+        private System.Windows.Forms.TextBox txtExample;
+        private System.Windows.Forms.Label lblExample;
+    }
+}
+
